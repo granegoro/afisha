2f1928c80a5921093cb2ec109a055436339fc9eb
Егор Старшелюк <granegoro@gmail.com>
2022-08-22 17:10:24 +0300

    CI added, findAll method added

---
+++ src/main/java/Afisha.java
@@ -0,0 +1,17 @@
+public class Afisha {
+    private String[] movies = new String[0];
+
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+}
